2ffdaf112351d84208d0a2f669a904788a349064
Smuzi <smuzzzzzzi@gmail.com>
2023-09-14 20:37:49 +0300

    prod

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post("/upload", checkAuth, upload.single("file"), (req, res) => {
 
 app.use(express.json())
 let allowCors = function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Origin', "http://smuzi-portfolio2.ru");
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
