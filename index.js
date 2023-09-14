bea621b0e4b2d01329f4c4a6332475c291215a00
Smuzi <smuzzzzzzi@gmail.com>
2023-09-14 20:39:56 +0300

    prod

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ app.post("/upload", checkAuth, upload.single("file"), (req, res) => {
 
 app.use(express.json())
 let allowCors = function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Origin', "smuzi-portfolio2.ru");
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
     }
 
