062fed6ff09c325daeac5779b360689227a10e6d
Smuzi <smuzzzzzzi@gmail.com>
2023-09-14 20:45:35 +0300

    prod

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post("/upload", checkAuth, upload.single("file"), (req, res) => {
 
 app.use(express.json())
 let allowCors = function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', "http://45.84.226.30:5000");
+        res.header('Access-Control-Allow-Origin', "45.84.226.30:5000");
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
